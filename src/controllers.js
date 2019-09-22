1c18f8ca5a132a2289c86ecb2a5974b2b32b29c1
John Afolayan <jnafolayan@gmail.com>
2019-09-22 23:04:32 +0100

    Fix npm start script

--- src/controllers.js
+++ src/controllers.js
@@ -48,6 +48,8 @@ export const decodeImage = [
         buffer: req.file.buffer
       });
 
+      console.log(req.body);
+
       res.status(200).json({
         message: decryptText(result, req.body.secretKey)
       });
